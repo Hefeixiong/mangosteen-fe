4c9234005f3aa28cdb314619c524c17b12b88546
hefx <1074535830@qq.com>
2022-11-26 17:03:05 +0800

    feature: 创建欢迎界面的页面

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,7 +1,20 @@
 import { Bar } from "../components/Bar";
 import { Foo } from "../components/Foo";
+import { First } from "../components/welcome/First";
+import { Forth } from "../components/welcome/Forth";
+import { Second } from "../components/welcome/Second";
+import { Third } from "../components/welcome/Third";
 
 export const routes = [
   { path: "/", component: Foo },
   { path: "/bar", component: Bar },
+  {
+    path: "/welcome",
+    children: [
+      { path: "1", component: First },
+      { path: "2", component: Second },
+      { path: "3", component: Third },
+      { path: "4", component: Forth },
+    ],
+  },
 ];
