3778055ad3c3d568de1d5791246db33ea92f9446
hefx <1074535830@qq.com>
2022-11-26 20:04:47 +0800

    抽离并使用Welcomelayout 组件

--- src/components/welcome/First.tsx
+++ src/components/welcome/First.tsx
@@ -1,28 +1,33 @@
-import { defineComponent } from "vue";
-import s from "./First.module.scss";
+import { FunctionalComponent } from "vue";
+import s from "./WelcomeLayout.module.scss";
 import pig from "../../assets/icons/pig.svg";
 import { RouterLink } from "vue-router";
+import { WelcomeLayout } from "./WelcomeLayout";
 
-export const First = defineComponent({
-  setup: (props, context) => {
-    return () => (
-      <div class={s.wrapper}>
-        <div class={s.card}>
-          <img src={pig} />
+export const First: FunctionalComponent = () => {
+  return (
+    <WelcomeLayout>
+      {{
+        icon: () => <img src={pig} />,
+        title: () => (
           <h2>
             会挣钱
             <br />
             还要会省钱
           </h2>
-        </div>
-        <div class={s.actions}>
-          <RouterLink class={s.fake} to="/start">
-            跳过
-          </RouterLink>
-          <RouterLink to="/welcome/2">下一页</RouterLink>
-          <RouterLink to="/start">跳过</RouterLink>
-        </div>
-      </div>
-    );
-  },
-});
+        ),
+
+        buttons: () => (
+          <>
+            <RouterLink class={s.fake} to="/start">
+              跳过
+            </RouterLink>
+            <RouterLink to="/welcome/2">下一页</RouterLink>
+            <RouterLink to="/start">跳过</RouterLink>
+          </>
+        ),
+      }}
+    </WelcomeLayout>
+  );
+};
+First.displayName = "First";
