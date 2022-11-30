56712b00d5cfbf1bc8633c3eebb61601ba1b55e1
hefx <1074535830@qq.com>
2022-11-30 22:09:51 +0800

    useSwipe

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,17 +1,24 @@
-import { defineComponent, Transition, VNode } from "vue";
+import { defineComponent, ref, Transition, VNode, watchEffect } from "vue";
 import { RouteLocationNormalizedLoaded, RouterView } from "vue-router";
 import logo from "../assets/icons/mangosteen.svg";
+import { useSwipe } from "../hooks/useSwipt";
 import s from "./Welcome.module.scss";
 
 export const Welcome = defineComponent({
   setup: (props, context) => {
+    const main = ref<HTMLElement | null>(null)
+    const { direction, swiping } = useSwipe(main)
+    watchEffect(() => {
+      console.log(direction.value)
+      console.log(direction.value)
+    }) 
     return () => (
       <div class={s.wrapper}>
         <header>
           <img src={logo} />
           <h1>山竹记账</h1>
         </header>
-        <main class={s.main}>
+        <main class={s.main} ref={main}>
           <RouterView name="main">
           {({ Component: X, route: R }: { Component: VNode, route: RouteLocationNormalizedLoaded }) =>
              <Transition enterFromClass={s.slide_fade_enter_from} enterActiveClass={s.slide_fade_enter_active}
