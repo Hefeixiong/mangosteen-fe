3abcf836aab5cfb7afb63f439f804dc8ef125f6d
hefx <1074535830@qq.com>
2022-12-01 23:03:00 +0800

    优化代码

--- src/components/welcome/First.tsx
+++ src/components/welcome/First.tsx
@@ -5,7 +5,7 @@ import pig from "../../assets/icons/pig.svg";
 export const First: FunctionalComponent = () => {
   return (
     <div class={s.card}>
-      <img src={pig} />,
+      <img src={pig} />
       <h2>会挣钱<br />还要会省钱</h2>
     </div>
   );
--- src/components/welcome/Third.tsx
+++ src/components/welcome/Third.tsx
@@ -5,8 +5,12 @@ import chart from "../../assets/icons/chart.svg";
 export const Third: FunctionalComponent = () => {
   return (
     <div class={s.card}>
-      <img src={chart} />,
-      <h2>每日提醒<br />不遗漏每一笔账单</h2>
+      <img src={chart} />
+      <h2>
+        每日提醒
+        <br />
+        不遗漏每一笔账单
+      </h2>
     </div>
   );
 };
