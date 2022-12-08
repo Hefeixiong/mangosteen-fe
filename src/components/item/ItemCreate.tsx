4e8376175e3d3bd03bc095fbd44fcde3d387211a
hefx <1074535830@qq.com>
2022-12-08 18:04:00 +0800

    暂存：Tabs组件

--- src/components/item/ItemCreate.tsx
+++ src/components/item/ItemCreate.tsx
@@ -1,6 +1,7 @@
-import { defineComponent, PropType } from "vue";
+import { defineComponent, PropType, ref } from "vue";
 import { MainLayout } from "../../layouts/MainLayout";
 import { Icon } from "../../shared/Icon";
+import { Tab, Tabs } from "../../shared/Tabs";
 import s from "./ItemCreate.module.scss";
 export const ItemCreate = defineComponent({
   props: {
@@ -9,6 +10,7 @@ export const ItemCreate = defineComponent({
     },
   },
   setup: (props, context) => {
+    const refKind = ref("支出");
     return () => (
       <MainLayout>
         {{
@@ -16,7 +18,10 @@ export const ItemCreate = defineComponent({
           icon: () => <Icon name="left" />,
           default: () => (
             <>
-              <div>main</div>
+              <Tabs v-modle:selected={refKind.value}>
+                <Tab name="支出">icon 列表</Tab>
+                <Tab name="收入">icon 列表2</Tab>
+              </Tabs>
             </>
           ),
         }}
