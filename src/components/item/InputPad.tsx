b5643e715747c5cda32d50f83c5332a4fc415eb7
hefx <1074535830@qq.com>
2022-12-12 16:52:14 +0800

    暂存；数字键盘

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -1,5 +1,6 @@
-import { defineComponent, PropType } from "vue";
+import { defineComponent, PropType, ref } from "vue";
 import { Icon } from "../../shared/Icon";
+import { time } from "../../shared/time";
 import s from "./InputPad.module.scss";
 export const InputPad = defineComponent({
   props: {
@@ -8,6 +9,9 @@ export const InputPad = defineComponent({
     },
   },
   setup: (props, context) => {
+    const refDate = ref<Date>();
+    const now = new Date();
+    console.log("now");
     const buttons = [
       { text: "1", onclick: () => {} },
       { text: "2", onclick: () => {} },
@@ -31,7 +35,9 @@ export const InputPad = defineComponent({
         <div class={s.dateAndAmount}>
           <span class={s.date}>
             <Icon name="date" class={s.icon} />
-            <span>2022-12-01</span>
+            <span>
+              <input type="date" value={time(now).format()} />
+            </span>
           </span>
           <span class={s.amount}>199.00</span>
         </div>
