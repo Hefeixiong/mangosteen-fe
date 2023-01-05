1fda18821b8040174993aae8f263159d16cce5d2
hefx <1074535830@qq.com>
2023-01-05 10:49:36 +0800

    扩展了 Tabs 组件，支持 classPrefix

--- src/shared/Tabs.tsx
+++ src/shared/Tabs.tsx
@@ -2,9 +2,10 @@ import { defineComponent, PropType } from "vue";
 import s from "./Tabs.module.scss";
 export const Tabs = defineComponent({
   props: {
+    classPrefix: { type: String },
     selected: {
       type: String as PropType<string>,
-      // required: true,
+      required: false,
     },
   },
   setup: (props, context) => {
@@ -16,12 +17,16 @@ export const Tabs = defineComponent({
           throw new Error("<Tabs> only accepts <Tab> as children");
         }
       }
+      const cp = props.classPrefix;
       return (
-        <div class={s.tabs}>
-          <ol class={s.tabs_nav}>
+        <div class={[s.tabs, cp + "_tabs"]}>
+          <ol class={[s.tabs_nav, cp + "_tabs_nav"]}>
             {tabs.map((item) => (
               <li
-                class={item.props?.name === props.selected ? s.selected : ""}
+                class={[
+                  item.props?.name === props.selected ? s.selected : "",
+                  cp + "_tabs_nav_item",
+                ]}
                 onClick={() =>
                   context.emit("update:selected", item.props?.name)
                 }
