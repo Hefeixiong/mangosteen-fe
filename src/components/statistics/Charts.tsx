cdbcb203c1ebca20aa2553c4de2131a4114024f1
hefx <1074535830@qq.com>
2023-01-09 17:47:29 +0800

    完成第三个图表

--- src/components/statistics/Charts.tsx
+++ src/components/statistics/Charts.tsx
@@ -1,5 +1,12 @@
 import * as echarts from "echarts";
-import { defineComponent, onMounted, PropType, ref } from "vue";
+import {
+  computed,
+  defineComponent,
+  onMounted,
+  PropType,
+  reactive,
+  ref,
+} from "vue";
 import { FormItem } from "../../shared/Form";
 import s from "./Charts.module.scss";
 export const Charts = defineComponent({
@@ -17,6 +24,18 @@ export const Charts = defineComponent({
     const category = ref("expenses");
     const refDiv = ref<HTMLDivElement>();
     const refDiv2 = ref<HTMLDivElement>();
+    const data3 = reactive([
+      { tag: { id: 1, name: "房租", sign: "x" }, amount: 3000 },
+      { tag: { id: 2, name: "吃饭", sign: "x" }, amount: 1000 },
+      { tag: { id: 3, name: "娱乐", sign: "x" }, amount: 900 },
+    ]);
+    const betterData3 = computed(() => {
+      const total = data3.reduce((sum, item) => sum + item.amount, 0);
+      return data3.map((item) => ({
+        ...item,
+        percent: Math.round((item.amount / total) * 100) + "%",
+      }));
+    });
     onMounted(() => {
       if (refDiv.value === undefined) {
         return;
@@ -82,6 +101,26 @@ export const Charts = defineComponent({
         />
         <div class={s.demo} ref={refDiv}></div>
         <div class={s.demo2} ref={refDiv2}></div>
+        <div class={s.demo3}>
+          {betterData3.value.map(({ tag, amount, percent }) => {
+            return (
+              <div class={s.topItem}>
+                <div class={s.sign}>{tag.sign}</div>
+                <div class={s.bar_wrapper}>
+                  <div class={s.bar_text}>
+                    <span>
+                      {tag.name} - {percent}
+                    </span>
+                    <span>¥{amount}</span>
+                  </div>
+                  <div class={s.bar}>
+                    <div class={s.bar_inner}></div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   },
